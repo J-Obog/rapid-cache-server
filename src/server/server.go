a3468e9692a7b8076c8a733d147284482e2bc5e9
J-Obog <jobogbaimhe@gmail.com>
2025-08-30 12:54:14 -0400

    adding base server initializion

--- src/server/server.go
+++ src/server/server.go
@@ -1,13 +1,38 @@
 package server
 
-import "github.com/gorilla/mux"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Server struct {
-	r mux.Router
+	r *mux.Router
 }
 
 func NewServer() *Server {
-	return &Server{
-		r: *mux.NewRouter(),
+	server := &Server{
+		r: mux.NewRouter(),
 	}
+
+	server.initializeRouter()
+	return server
+}
+
+func (s *Server) initializeRouter() {
+	s.r.HandleFunc("/set", s.handleKeySet).Methods("POST")
+	s.r.HandleFunc("/del", s.handleKeyDelete).Methods("POST")
+	s.r.HandleFunc("/diff", s.handleDiff).Methods("POST")
+}
+
+func (s *Server) handleKeySet(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) handleKeyDelete(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) handleDiff(w http.ResponseWriter, r *http.Request) {
+
 }
