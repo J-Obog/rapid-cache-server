624983d374d6abfdbebece0dab2b3f438442afdf
J-Obog <jobogbaimhe@gmail.com>
2025-08-30 13:16:28 -0400

    adding initial implementation for key set

--- src/server/server.go
+++ src/server/server.go
@@ -1,18 +1,24 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/J-Obog/rapid-cache-server/src/command"
 	"github.com/gorilla/mux"
 )
 
 type Server struct {
-	r *mux.Router
+	r   *mux.Router
+	aol command.AppendOnlyCommandList
 }
 
 func NewServer() *Server {
 	server := &Server{
-		r: mux.NewRouter(),
+		r:   mux.NewRouter(),
+		aol: *command.NewAppendOnlyCommandList(),
 	}
 
 	server.initializeRouter()
@@ -26,7 +32,35 @@ func (s *Server) initializeRouter() {
 }
 
 func (s *Server) handleKeySet(w http.ResponseWriter, r *http.Request) {
+	timeNow := time.Now()
 
+	data := make(map[string]interface{})
+
+	bodyDecoder := json.NewDecoder(r.Body)
+	bodyDecoder.Decode(data) //TODO: Handle error
+
+	key := data["k"].(string)
+	val := data["v"].(string)
+
+	exp := int64(-1)
+
+	if expMsIface, exists := data["expMs"]; exists {
+		expMs, _ := expMsIface.(int64)
+		exp = expMs
+	}
+
+	s.aol.Append(command.Command{
+		Name:      command.CommandNameSet,
+		Key:       key,
+		Timestamp: timeNow,
+		Seed:      "",
+		Params: map[string]string{
+			"value":     val,
+			"expiresAt": strconv.FormatInt(exp, 10),
+		},
+	})
+
+	w.WriteHeader(http.StatusAccepted)
 }
 
 func (s *Server) handleKeyDelete(w http.ResponseWriter, r *http.Request) {
