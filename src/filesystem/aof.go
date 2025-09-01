1c52a7b6629dbbcdc9a70829fea299a2640fcbaf
J-Obog <jobogbaimhe@gmail.com>
2025-09-01 09:50:47 -0400

    implementing append method

--- src/filesystem/aof.go
+++ src/filesystem/aof.go
@@ -1,12 +1,27 @@
 package filesystem
 
+import (
+	"bytes"
+	"encoding/gob"
+	"os"
+)
+
 type AppendOnlyStateChangeFile struct {
+	file *os.File
 }
 
 func (aof *AppendOnlyStateChangeFile) Read() ([]StateChange, error) {
 	return nil, nil
 }
 
-func (aof *AppendOnlyStateChangeFile) Append(newCommand StateChange) error {
-	return nil
+func (aof *AppendOnlyStateChangeFile) Append(newStateChange StateChange) error {
+	var buf bytes.Buffer
+
+	enc := gob.NewEncoder(&buf)
+	if err := enc.Encode(newStateChange); err != nil {
+		return err
+	}
+
+	_, err := aof.file.Write(buf.Bytes())
+	return err
 }
